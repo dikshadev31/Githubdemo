19663f7054bcb933c2d32971ab75d571c120768c
dikshadev31 <diksha.dev@fidelity.co.in>
2023-09-20 14:33:45 +0530

    commit

---
+++ App/src/com/app/Paypal.java
@@ -0,0 +1,7 @@
+package com.app;
+
+public class Paypal {
+	public int sum1(int n1,int n2) {
+		return (n1+n2);
+	}
+}
---
+++ App/src/com/app/UpiPayment.java
@@ -0,0 +1,7 @@
+package com.app;
+
+public class UpiPayment {
+public int sum(int n1,int n2) {
+	return (n1+n2);
+}
+}
